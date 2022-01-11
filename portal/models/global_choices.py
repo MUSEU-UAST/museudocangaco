234d55cdb7442b882e2bcbea55b6e52669a6f325
heldonjose <heldonjose@gmail.com>
2022-01-10 23:45:21 -0300

    servidor

--- portal/models/global_choices.py
+++ portal/models/global_choices.py
@@ -1,3 +1,3 @@
-from django.contrib.auth.models import User
-
-USERS = tuple(map(lambda obj: (obj.username, obj.username), User.objects.all()))
+# from django.contrib.auth.models import User
+#
+# USERS = tuple(map(lambda obj: (obj.username, obj.username), User.objects.all()))
